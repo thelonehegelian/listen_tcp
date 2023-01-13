e053a8d3844c0a48c80d8a14f431746c1b5f8606
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 03:45:02 +0000

    Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+- Once the message is sent the server closes
+- Send message to the server: `echo "Lelho Rowld!" | nc -v 127.0.0.1 <port_number>`
+TODO: 
+---
+
+- [ ] add loop so the server keeps running until it is closed 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use std::io::{Error, Read};
 use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
-
+//  the program will listen until a request is made
 fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+    let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0);
+    let listener = TcpListener::bind(addr)?;
+    // assigns random port
+    let port = listener.local_addr()?;
+    println!("{}", port);
+    println!("Listening on port {}", port.port());
+    // accept a single connection, creates a new TcpStream
+    let (mut stream, addr) = listener.accept()?;
+    println!("Connection received from {}", addr);
+    // read incoming to string
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+    // print the incoming message
+    println!("Message received: {}", buffer);
     Ok(())
 }
